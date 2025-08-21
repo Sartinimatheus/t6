43c01f649aaf036ffee1fa2662a0747258bfb47f
Sartinimatheus <matheussartini214@gmail.com>
2025-08-21 19:09:53 -0300

    Add files via upload

---
+++ BACK-END_T6/desafio3/main.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+ <meta charset="UTF-8">
+ <title>Desafio com Include</title>
+</head>
+<body>
+ <?php include 'functions.php'; ?>
+ <h2>Desafio com Include</h2>
+ <?php
+$num1 = 14;
+$num2 = 7;
+ echo "<p>A soma de $num1 e $num2 é: " . calcularSoma($num1, $num2) . "</p>";
+ ?>
+</body>
+</html>
